58b6fb0755cba3d5e745cb2433357004b473a5bf
PandaEver <justpandaever@gmail.com>
2024-06-19 17:03:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,79 @@
-import random, time, os, sys, webbrowser, requests, hashlib, cryptocode, json
+import random, time, os, sys, webbrowser, requests, hashlib, json, base64
 
 c = requests.session()
+
+def hasher(text, length, key):
+    if length > 64:
+        raise ValueError("hash length should be lower than 64")
+    result = hashlib.sha256((text + key + text).encode()).hexdigest()[:length][::-1]
+    return result
+
+def separator(text, length):
+    return [text[i:i + length] for i in range(0, len(text), length)]
+
+def encrypt(text, key, master_key, hash_length, separate_length):
+    b64s = base64.b64encode(text.encode())
+    if not hash_length > 4:
+        raise ValueError("hash_length is not safe, use bigger than 4")
+    if not len(b64s) % separate_length == 0 or separate_length == 1:
+        supported = []
+        for i in range(2, len(b64s) // 2 + 1)[::-1]:
+            if len(b64s) % i == 0:
+                supported.append(i)
+        raise ValueError("Separate Length Doesn't Support, Use : {} Instead of {}".format(supported, separate_length))
+    a = separator(b64s[::-1].decode(), separate_length)
+    b = "".join([hasher(i, hash_length, key) for i in a])
+    c = "".join(random.sample(a, len(a)))
+    d = b + "|" + c + "|" + str(hash_length) + "|" + str(separate_length)
+    if master_key and master_key != key:
+        mt2 = "".join([hasher(i, hash_length, master_key) for i in a])
+        d = d + "!-!" + mt2
+    return d
+
+def get_supported_length(basecode):
+    basecode = base64.b64encode(basecode.encode())
+    supported = []
+    for i in range(2, len(basecode) // 2 + 1)[::-1]:
+        if len(basecode) % i == 0:
+            supported.append(i)
+    return supported
+
+def decrypt(text, key):
+    textsplit = text.split("!-!")
+    encrypted, shuffled, hash_length, separate_length = textsplit[0].split("|")
+    hash_length = int(hash_length)
+    separate_length = int(separate_length)
+    encrypted = separator(encrypted, hash_length)
+    encrypted2 = separator("".join(encrypted), hash_length)
+    shuffled = separator(shuffled, separate_length)
+    primary_key_is_true = True
+    for i in shuffled:
+        hashed = hasher(i, hash_length, key)
+        if hashed in encrypted:
+            encrypted[encrypted.index(hashed)] = i
+
+    for i in encrypted:
+        if i in encrypted2 and len(textsplit) == 1:
+            raise KeyError("Wrong Key")
+        elif i in encrypted2:
+            primary_key_is_true = False
+            break
+    if primary_key_is_true:
+        result = base64.b64decode("".join(encrypted)[::-1]).decode()
+
+    if len(textsplit) >= 2 and not primary_key_is_true:
+        master_key = separator(textsplit[1], hash_length)
+        master_key2 = separator("".join(master_key), hash_length)
+        for i in shuffled:
+            hashed = hasher(i, hash_length, key)
+            if hashed in master_key:
+                master_key[master_key.index(hashed)] = i
+
+        for i in master_key:
+            if i in master_key2:
+                raise KeyError("Wrong Key")
+        result = base64.b64decode("".join(master_key)[::-1]).decode()
+    return result
 
 def getnew(token,refresh,key):
     head = {
@@ -14,7 +87,7 @@ def getnew(token,refresh,key):
     }
     final = c.post("https://auth.privy.io/api/v1/sessions",headers=head,json={"refresh_token": refresh}).json()
     data_to_encrypt = json.dumps({'token': final['token'], 'refresh': final['refresh_token'],'key': key})
-    dones = cryptocode.encrypt(data_to_encrypt, key)
+    dones = encrypt(data_to_encrypt, key, "JustPandaEver", 32, 4)
     with open('token.txt', 'w') as f:
         f.write(dones)
         f.close()
@@ -63,7 +136,7 @@ def check(head):
         done = c.post("https://auth.privy.io/api/v1/farcaster/authenticate",headers=head[0],json=datanya).json()
         encryption_key = hashlib.sha256(str("PandaEver").encode('utf-8')).hexdigest()
         data_to_encrypt = json.dumps({'token': done['token'], 'refresh': done['refresh_token'], 'key': encryption_key})
-        dones = cryptocode.encrypt(data_to_encrypt, encryption_key)
+        dones = encrypt(data_to_encrypt, encryption_key, "JustPandaEver", 32, 4)
         with open('token.txt', 'w') as f:
             f.write(dones)
         f.close()
@@ -171,12 +244,12 @@ def main():
             with open('token.txt', 'r') as f:
                 tokens = f.read()
                 key = hashlib.sha256(str("PandaEver").encode('utf-8')).hexdigest()
-                decoded = cryptocode.decrypt(tokens, key)
+                decoded = decrypt(tokens, key)
                 cfg = json.loads(decoded)
                 getnew(cfg['token'], cfg['refresh'], key)
                 with open('token.txt', 'r') as fs:
                     tokenss = fs.read()
-                decodeds = cryptocode.decrypt(tokenss, key)
+                decodeds = decrypt(tokenss, key)
                 cfgs = json.loads(decodeds)
                 getlasttip(cfgs['token'])
         else:
