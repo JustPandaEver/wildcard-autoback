536002e6d0f6da25576107c1498ba11145375f91
PandaEver <justpandaever@gmail.com>
2024-06-19 18:21:15 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+pkg update && pkg upgrade -y && pkg install pip python -y && pip install requests && git clone https://github.com/JustPandaEver/wildcard-autoback && cd wildcard-autoback && python main.py
